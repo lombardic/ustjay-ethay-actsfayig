354ca97f85cc41ce9b32cfbb80a16ba01cee36dd
Chris Lombardi <chris.m.lombardi@gmail.com>
2019-09-16 20:14:13 -0700

    Adding complted main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# Pig Latinizer
+# https://hidden-journey-62459.herokuapp.com/
+# Request URL = https://hidden-journey-62459.herokuapp.com/piglatinize/
+# Response Header = Location
+# Request Method = POST
+
 import os
 
 import requests
@@ -5,7 +11,6 @@ from flask import Flask, send_file, Response
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-
 
 def get_fact():
 
@@ -16,13 +21,22 @@ def get_fact():
 
     return facts[0].getText()
 
+def pig_latinize(fact):
+
+    payload = {'input_text': fact}
+    response = requests.post("https://hidden-journey-62459.herokuapp.com/piglatinize/",
+                             data=payload, allow_redirects=False)
+    return response.headers['Location']
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    latinized = pig_latinize(fact)
 
+    page = f'<a href="{latinized}">{latinized}</a>'
+    return page
+    #return latinized
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
